574db019bac1b37c64f2e94e29abc1f0e1530124
John Blakkan <john1@blakkan.net>
2019-02-25 22:28:15 -0800

    Added environment to work on Rpi

--- Gemfile
+++ Gemfile
@@ -65,5 +65,8 @@ group :test do
   gem 'chromedriver-helper'
 end
 
+group :rpi do
+  gem 'sqlite3', '~> 1.3.6'
+end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+# Havt to comment this out; kills raspberry pi.
+#require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
