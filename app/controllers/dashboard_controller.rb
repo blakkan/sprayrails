f2fb8e4135889e45073c145ef932401b0d930aa0
John Blakkan <john1@blakkan.net>
2019-03-11 22:42:07 -0700

    Added logging

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,9 @@ if ENV['RAILS_ENV'] =~ /rpi/
 require 'rpi_gpio'
 end
 
+require 'net/http'
+require 'json'
+
 class DashboardController < ApplicationController
 
   def display_main_screen
@@ -61,63 +64,72 @@ class DashboardController < ApplicationController
       #puts "About to read\n"
       @classification = $classifier.readline
       @class_items = @classification.split(/ probability: /)
+
+      #at this point, @class_items[0] has the plant, and @class_items[1] has the probabilitiy
+
+
       #puts "Did the read\n"
       if (@class_items[0] =~ /(arigo)|(orning)/ ) && ( ENV['RAILS_ENV'] =~ /rpi/ )
 
+	        RPi::GPIO.set_numbering :bcm
 
-	RPi::GPIO.set_numbering :bcm
+	        RPi::GPIO.setup 23, :as => :input, :pull => :down
+	        RPi::GPIO.setup 24, :as => :input, :pull => :up
 
-	RPi::GPIO.setup 23, :as => :input, :pull => :down
-	RPi::GPIO.setup 24, :as => :input, :pull => :up
+	        RPi::GPIO.setup 21, :as => :output
 
+          RPi::GPIO.set_high 21
+          sleep 1
+          RPi::GPIO.set_low 21
 
-	RPi::GPIO.setup 21, :as => :output
-
-
-        RPi::GPIO.set_high 21
-        sleep 1
-        RPi::GPIO.set_low 21
-
-
-	RPi::GPIO.reset
+	        RPi::GPIO.reset
 
       end
+
+      # Now update the counts
+      old_count_record = Kv.find_by( key: @class_items[0])
+      old_count_number = old_count_record.value
+      old_count_record.update_attribute( value: (old_count_number.to_i + 1).to_s )
+
+      #FIXME And update the pump time (just one second, should be from database )
+      old_pump_record = Kv.find_by( key: 'pump_time')
+      old_pump_value = old_pump_record.value
+      old_pump_record.update_attribute( value: (old_pump_value + 1.0))
     end
 
-
     render :display_current_image_classification
+
   end
 
 
   def update_setup
 
     if params.key?("run_pump") &&
-  	params[:run_pump] == "run_pump" && Rails.environment.rpi?
+  	  params[:run_pump] == "run_pump" && Rails.environment.rpi?
 
 	#puts "run pump"
 
-	RPi::GPIO.set_numbering :bcm
+    	RPi::GPIO.set_numbering :bcm
 
-	RPi::GPIO.setup 23, :as => :input, :pull => :down
-	RPi::GPIO.setup 24, :as => :input, :pull => :up
+	    RPi::GPIO.setup 23, :as => :input, :pull => :down
+	    RPi::GPIO.setup 24, :as => :input, :pull => :up
 
+	    RPi::GPIO.setup 21, :as => :output
 
-	RPi::GPIO.setup 21, :as => :output
+      RPi::GPIO.set_high 21
+      sleep 5
+      RPi::GPIO.set_low 21
 
-
-        RPi::GPIO.set_high 21
-        sleep 5
-        RPi::GPIO.set_low 21
-
-
-	RPi::GPIO.reset
+	    RPi::GPIO.reset
 
     end
 
     Kv.find_by( key: "sn").update( value: params[:sn] )
     Kv.find_by( key: "latitude").update( value: params[:latitude] )
     Kv.find_by( key: "longitude").update( value: params[:longitude] )
+
     render :display_setup_screen
+
   end
 
   def update_dosage
@@ -131,11 +143,49 @@ class DashboardController < ApplicationController
 
   def transmit_and_clear_counts
 
-    # And clear all
+    # send it to the fixed address location
+    uri = URI("http://127.0.0.1:3001/upload_data_rest")
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})  #managing our own conversion
+    assembling_body = { "json_entry" => {}}
+
+    # Pick out values, clearing as we go
     [ "radish", "pea", "marigold", "morning_glory"]. each do |plant_count|
-      Kv.find_by( key: plant_count).update_attribute( "value", "0")
+      assembling_body["json_entry"][plant_count] = Kv.find_by( key: plant_count).value
     end
-    Kv.find_by( key: "pump_time").update_attribute( "value", "0.0")
+    assembling_body["json_entry"]["pump_time"] = Kv.find_by( key: "pump_time").value
+
+    #Add lat, long, and SN
+    assembling_body["json_entry"]["serial_number"] = Kv.find_by( key: "sn").value
+    assembling_body["json_entry"]["lat"] = Kv.find_by( key: "latitude").value
+    assembling_body["json_entry"]["lon"] = Kv.find_by( key: "longitude").value
+    assembling_body["json_entry"]["timestamp"] = Time.now.utc.iso8601
+
+    req.body = assembling_body.to_json
+
+    p req.body
+
+    # Now transfer
+    begin
+      puts "About to send"
+      res = http.request(req)
+      puts "Sent without exception"
+
+      # And clear only if successful
+      [ "radish", "pea", "marigold", "morning_glory"]. each do |plant_count|
+        Kv.find_by( key: plant_count).update_attribute( "value", "0")
+      end
+      Kv.find_by( key: "pump_time").update_attribute( "value", "0.0")
+
+      #puts "repsonse #{res.body}"
+    rescue => e
+      puts "EXCEPTION"
+      puts "failed #{e}"
+    end
+
+
+
+
     render :display_statistics_screen
   end
 
