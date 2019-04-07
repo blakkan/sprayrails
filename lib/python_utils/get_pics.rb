ceb792c00999b3da33ac2cd722a3a963a053a19b
John Blakkan <john.blakkan@berkeley.edu>
2019-04-07 15:39:32 -0700

    Added ruby get_pics.rb to convert storage files into sorted jpgs

---
+++ lib/python_utils/get_pics.rb
@@ -0,0 +1,11 @@
+
+# the -5 below is number of days ago.
+
+x=`find ../../storage/ -mtime -5 -type f -printf "%T@ %Tc %p\n" | sort -n`.split(/\n/)
+
+x.each_with_index do |line, i|
+ full_rel_name = line.split(/\s+/)[-1]
+ new_name = sprintf("IMG_LOG_%04d", i) + ".jpg"
+ system("cp #{full_rel_name} #{new_name}")
+end
+
